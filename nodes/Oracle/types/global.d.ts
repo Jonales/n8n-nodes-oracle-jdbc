5e1136a717149579748423bf411afabcd8dcb12e
Jônatas Meireles <jonales@gmail.com>
2025-08-16 18:44:49 -0300

    Create global.d.ts

---
+++ nodes/Oracle/types/global.d.ts
@@ -0,0 +1,12 @@
+declare global {
+  const java: any;
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production' | 'test';
+      ORACLE_HOME?: string;
+      TNS_ADMIN?: string;
+    }
+  }
+}
+
+export {};
